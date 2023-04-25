11cb20bf40d52bf0fb57a5452ade2a7891434258
Andrew Kotlov <andrewkotlov48@gmail.com>
2023-04-25 16:55:20 +0300

    test 3

--- app/public/script.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.ico_btn');
-let isMenuOpen = false;
-
-menuBtn.addEventListener('click', () => {
-  isMenuOpen = !isMenuOpen;
-  menuBtn.classList.toggle('is-active');
-});
